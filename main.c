efb65a5396db3cad2fd907139c289597bbc27022
Michael Fitzmayer <mail@michael-fitzmayer.de>
2018-04-29 10:25:45 +0200

    Add missing main.c update to commit e717370

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main()
 
     atexit(SDL_Quit);
 
-    Map *map = mapInit("res/maps/forest.tmx", video->height);
+    Map *map = mapInit("res/maps/forest.tmx");
     if (NULL == map)
     {
         execStatus = EXIT_FAILURE;
@@ -38,21 +38,23 @@ int main()
     }
     player->frameYoffset =  32;
     player->worldPosX    =  64;
-    player->worldPosY    = 424;
+    player->worldPosY    = 624;
 
     /* Note: The error handling isn't missing here.  There is simply no need to
      * quit the program if the music can't be played by some reason. */
-    Mixer *mixer    = mixerInit();
+    /*Mixer *mixer    = mixerInit();
     Music *music    = musicInit();
     music->filename = "res/music/creepy.ogg";
-    if (mixer) musicFadeIn(music, 5000);
+    if (mixer) musicFadeIn(music, 5000);*/
 
     uint16_t flags      = 0;
     int32_t  cameraPosX = 0;
-    int32_t  cameraPosY = 0;
+    int32_t  cameraPosY = map->map->height * map->map->tile_height - video->height;
 
     while (1)
     {
+        player->gid = mapGetGID(map->map->width, player->worldPosX, player->worldPosY);
+
         // Handle keyboard input.
         const uint8_t *keyState;
         SDL_PumpEvents();
@@ -61,9 +63,7 @@ int main()
 
         // Reset flag (in case no key is pressed).
         player->flags &= ~(1 << IN_MOTION);
-
         keyState = SDL_GetKeyboardState(NULL);
-        SDL_Delay(5);
 
         if (keyState[SDL_SCANCODE_Q]) goto quit;
         if (keyState[SDL_SCANCODE_A])
@@ -90,10 +90,15 @@ int main()
         else
         {
             cameraPosX = player->worldPosX - video->width  / 2;
-            cameraPosY = player->worldPosY - video->height / 2 - 32;
+            cameraPosY = player->worldPosY - video->height / 2 - 16;
         }
+        // Set camera boundaries to map size.
+        int32_t cameraMaxX = map->map->width  * map->map->tile_width  - video->width;
+        int32_t cameraMaxY = map->map->height * map->map->tile_height - video->height;
         if (cameraPosX < 0) cameraPosX = 0;
-        if (cameraPosY > 0) cameraPosY = 0;
+        if (cameraPosY < 0) cameraPosY = 0;
+        if (cameraPosX > cameraMaxX) cameraPosX = cameraMaxX;
+        if (cameraPosY > cameraMaxY) cameraPosY = cameraMaxY;
 
         // Render scene.
         if (-1 == mapRender(video->renderer, map, "Background", 1, 0, map->worldPosX - cameraPosX, map->worldPosY - cameraPosY))
@@ -122,11 +127,12 @@ int main()
 
         SDL_RenderPresent(video->renderer);
         SDL_RenderClear(video->renderer);
+        SDL_Delay(5);
     }
 
     quit:
-    musicFree(music);
-    mixerFree(mixer);
+    /*musicFree(music);
+    mixerFree(mixer);*/
     entityFree(player);
     mapFree(map);
     videoTerminate(video);
