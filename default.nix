61853c8d46a9915ad2f605c1a9ab446c253f3273
Michael Fitzmayer <mail@michael-fitzmayer.de>
2018-06-09 17:36:15 +0200

    Add obadz's fix to default.nix

--- default.nix
+++ default.nix
@@ -1,22 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.stdenv.mkDerivation {
+with pkgs;
+with stdenv.lib;
+
+let
+    makeSDLCFlags = lib.concatMap (p: [ "-I${getDev p}/include/SDL2" "-I${getDev p}/include" ]);
+    makeSDLDFlags = lib.concatMap (p: [ "-L${getLib p}/lib" ]);
+    SDLlibs = [ SDL2 SDL2_image SDL2_mixer ];
+
+in pkgs.stdenv.mkDerivation {
     name = "rainbow-joe-git";
+    src = ./.;
 
-    src = pkgs.fetchFromGitHub {
-        owner = "mupfelofen-de";
-        repo = "rainbow-joe";
-        rev = "68cd98ac0cdbeb8f78e650b13ae0efa5c726f56e";
-        sha256 = "0w1jbkshzxvphmis1gsb91v5qk1ckjdhcg1ywfhgbqw73s87vn2g";
-    };
-
-    buildInputs = with pkgs; [
-        libxml2
-        SDL2
-        SDL2_image
-        SDL2_mixer
-        zlib
-    ];
+    NIX_CFLAGS_COMPILE = (makeSDLCFlags SDLlibs) ++ [ "-I${getDev zlib}/include" "-I${getDev libxml2}/include/libxml2" ];
+    NIX_CFLAGS_LINK    = (makeSDLDFlags SDLlibs) ++ [ "-L${getLib zlib}/lib" "-L${getLib libxml2}/lib" "-L${getLib lzma}/lib" "-L${getLib icu}/lib" ];
 
     installPhase = ''
         install -Dsm 755 rainbow-joe $out/bin/rainbow-joe
